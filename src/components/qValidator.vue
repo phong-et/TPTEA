730f0b8f62756d51944c3673a38d0c735bfa66f2
Phillip <phillip7.et@gmail.com>
2018-09-06 18:32:15 +0700

    Routing, Register,  Database(Customers)
    Add logic routing for each tab page 
    Add page register, q-Validator and complete reg function 
    Change "add" column to "address" of seeder, migration, model

---
+++ src/components/qValidator.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="dirty">
+    <div class="error" v-if="show">{{msg}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'QValidator',
+  props: {
+    dirty: {
+      default: false,
+      type: Boolean,
+    },
+    show: {
+      default: false,
+      type: Boolean,
+    },
+    msg: {
+      default: 'Input value does not match the validation',
+      type: String,
+    },
+  },
+}
+</script>
+<style>
+.error {
+  padding: 10px;
+  font-size: 12px;
+  font-style: italic;
+  color: red;
+}
+</style>
