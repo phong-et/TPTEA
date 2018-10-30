68c2ced30a338f44666937e781d556e57f6217fb
Seven <alvin.elitetech@gmail.com>
2018-10-30 16:03:19 +0700

    Create grid gift card

--- src/store/giftcard/actions.js
+++ src/store/giftcard/actions.js
@@ -1,5 +1,25 @@
-import {_procError, _post, _procAlert} from '../../util/common'
+import {_procError, _post, _get, _procAlert} from '../../util/common'
 import _ from 'lodash'
+
+export const fetchGiftCards = ({commit}) => {
+  commit('setIsLoading', true)
+  _get(`{
+    fetchGiftCards {
+      id
+      code
+      username
+    }
+  }`)
+    .then(({data}) => {
+      _procAlert(data, true)
+      commit('setRecs', data.fetchGiftCards)
+      commit('setIsLoading', false)
+    })
+    .catch(err => {
+      _procError(err)
+      commit('setIsLoading', false)
+    })
+}
 
 export const updateGiftCard = ({commit, getters}) => {
   commit('setIsLoading', true)
