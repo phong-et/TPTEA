334394b232697d3b1d06bec3dd58f4eb0135e7de
Phillip <phillip7.et@gmail.com>
2018-11-02 18:21:05 +0700

    remove some codes

--- src/components/CreateGiftCardModal.vue
+++ src/components/CreateGiftCardModal.vue
@@ -2,7 +2,7 @@
   <q-modal v-model="isModalOpened" no-backdrop-dismiss no-esc-dismiss>
     <q-modal-layout>
       <q-toolbar color="primary">
-        <q-btn class="modal-title" flat color="white" icon="close" @click="isModalOpened=false"></q-btn>
+        <q-btn class="modal-title" flat icon="close" @click="isModalOpened=false"></q-btn>
       </q-toolbar>
       <div class="q-pa-lg">
         <div class="row">
@@ -10,13 +10,13 @@
             <q-input v-model="expiry" type="number" suffix="days" color="secondary" :error="$v.expiry.$error" />
             <et-validator :dirty="$v.expiry.$dirty" :show="!$v.expiry.required" msg="expiry is required" />
             <et-validator :dirty="$v.expiry.$dirty" :show="!$v.expiry.integer" msg="expiry must have a interger" />
-            <et-validator :dirty="$v.expiry.$dirty" :show="!$v.expiry.between" msg="expiry is <=1 and <= 100 " />
+            <et-validator :dirty="$v.expiry.$dirty" :show="!$v.expiry.between" msg="expiry is <=1 and <= 1000 " />
           </q-field>
           <q-field color="secondary" class="q-mb-md col-11" label-width="3" icon="attach_money" label="Amout">
             <q-input v-model="amount" type="number" prefix="$" color="secondary" :error="$v.amount.$error" />
             <et-validator :dirty="$v.amount.$dirty" :show="!$v.amount.required" msg="amount is required" />
             <et-validator :dirty="$v.amount.$dirty" :show="!$v.amount.numeric" msg="amount must have a numeric" />
-            <et-validator :dirty="$v.amount.$dirty" :show="!$v.amount.between" msg="amount is is <=1 and <= 100" />
+            <et-validator :dirty="$v.amount.$dirty" :show="!$v.amount.between" msg="amount is is <=1 and <= 1000" />
           </q-field>
         </div>
         <div class="row q-py-md gutter-x-sm justify-center" style="background:#fff">
@@ -29,13 +29,11 @@
   </q-modal>
 </template>
 <script>
-import etModal from './EtModal'
 import {mapGetters, mapActions} from 'vuex'
 import etValidator from './Validator'
 import {required, between, numeric, integer} from 'vuelidate/lib/validators'
 export default {
   components: {
-    etModal,
     etValidator,
   },
   data() {
