8eb7791ec63460c1f618ad4f3b4ada91782081d7
Phillip <phillip7.et@gmail.com>
2019-03-01 10:49:16 +0700

    fix ETModal - disable (+) button when getEdittingRec is empty

--- src/pages/admin/Modifiers.vue
+++ src/pages/admin/Modifiers.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <et-grid type="adminmodifier" aliasType="AdminModifiers" />
-    <et-modal type="adminmodifier" aliasType="AdminModifiers" />
+    <et-modal type="adminmodifier" aliasType="AdminModifier" />
   </div>
 </template>
 <script>
